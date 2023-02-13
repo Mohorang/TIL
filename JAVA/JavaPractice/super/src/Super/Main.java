2b69213b13dd0e2692d1c242aad3cddb1d33df5e
DESKTOP-DQ7J780\dlwns <dlwnsgh253@gmail.com>
2023-02-13 18:00:21 +0900

    super(); 생성자 관련 간단코드작성

---
+++ JAVA/JavaPractice/super/src/Super/Main.java
@@ -0,0 +1,31 @@
+package Super;
+
+class Point{
+    int x;
+    int y;
+
+    Point(){
+        this(1,2);
+    }
+    Point(int x , int y){
+        this.x = x;
+        this.y = y;
+    }
+
+}
+class Point3D extends Point{
+    int z;
+
+    Point3D(int z){
+        //super(); 클래스가 자동 호출됨.
+        this.x = 2;
+        this.y = 3;
+        this.z = z;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Point3D p3 = new Point3D(3);
+        System.out.println("p3.x = " + p3.x + " p3.y = " + p3.y + " p3.z = " + p3.z);
+    }
+}
