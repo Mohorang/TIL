a25fb387036446f0e370ca5de4ec8eb609dc296e
DESKTOP-DQ7J780\dlwns <dlwnsgh253@gmail.com>
2023-02-20 18:47:12 +0900

    디렉토리명 변경

---
+++ JAVA/JavaPractice/JavaJeongSeok/src/Ch07/Interface/ex3/InterfaceTest.java
@@ -0,0 +1,32 @@
+package Ch07.Interface.ex3;
+
+public class InterfaceTest {
+    public static void main(String[] args) {
+        A a = new A();
+        a.methodA();
+    }
+}
+class A{
+    void methodA(){
+        I i = InstanceManager.getInstance();
+        i.methodB();
+        System.out.println(i.toString());
+    }
+}
+interface I {
+    void methodB();
+}
+class B implements  I{
+    @Override
+    public void methodB() {
+
+    }
+
+    public String toString(){ return "class B";
+    }
+}
+class InstanceManager{
+    public static I getInstance(){
+        return new B();
+    }
+}
